4ef3f3a6c6cb7c9270fc4c191f8f966c08a487f8
Salman S <146117732+Salman-id85@users.noreply.github.com>
2024-12-30 17:42:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 (function () {
+    // Handle button click events to toggle active classes
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
+            // Remove active-btn class from the previous active button
             document.querySelector(".active-btn").classList.remove("active-btn");
+            // Add active-btn class to the clicked button
             this.classList.add("active-btn");
+            // Remove active class from the previously active content
             document.querySelector(".active").classList.remove("active");
+            // Add active class to the new content
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
+
+    // Handle theme button click to toggle light mode
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
 })();
